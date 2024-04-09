d8ee05acaa028dd61a5dbdeff72965b176783460
3138176078@qq.com <3138176078@qq.com>
2024-04-08 18:24:22 -0700

    abstract class primitives

---
+++ 2d/primitives.py
@@ -0,0 +1,20 @@
+from abc import ABC, abstractmethod
+class Primitives:
+    def __init__(self, shapeType, colour, alpha, isBrushed):
+        self.colour = colour
+        self.alpha = alpha
+        self.isBrushed = isBrushed
+        self.shapeType = shapeType
+    @abstractmethod    
+    def info(self):
+        pass
+    
+    def fill(self):
+        pass
+    
+    def generate(self):
+        pass
+    
+    
+    
+    
